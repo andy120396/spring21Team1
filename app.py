db33bafb73f2e2005a33c35245ff9f98e332153d
Andy Liu <akl5294@psu.edu>
2021-03-31 18:02:02 -0400

    minor change to text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return "<h1>Rock Paper Scissor simulator</h1><p>This site is a prototype API for rock paper scissor game.</p>"
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
