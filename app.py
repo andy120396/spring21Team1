d7e62add4b753d1dea39d38ab93fffc2b184d4ef
Andy Liu <akl5294@psu.edu>
2021-03-31 18:11:01 -0400

    add templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Rock Paper Scissor simulator</h1><p>This site is a prototype API for rock paper scissor game.</p>"
+    return render_template(index.html)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
