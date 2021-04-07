3c5bc521f5db59597ea1c2ef51651c6b2850fbc5
Andy Liu <akl5294@psu.edu>
2021-04-06 23:48:55 -0400

    edits to app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, Blueprint
+from dotenv import load_dotenv
+from game_utils.rock_paper_scissors import *
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
 
-
+def create_app():
+    load_dotenv()
+    app_enviro = os.environ.get("FLASK.ENV", "development")
+    secret_key = os.environ.get("SECRET_KEY", "my super secret")
+    testing = False
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(ENV=app_env, SECRET_KEY=secret_key, TESTING=testing)
+    app.register_blueprint(game_routes)
+    return app
 
 if __name__ == '__main__':
-	app.run()
+    rpc_app = create_app()
+	rps_app.run()
 
 #Andy Liu
 #Anzar Anwar
